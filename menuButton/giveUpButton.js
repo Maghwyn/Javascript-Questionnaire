d6bb310112078b82a3c94558e7722e8bbaf7e97c
Maghwyn <christopher.samson@edu.esiee-it.fr>
2021-10-22 10:33:28 +0200

    Update the GiveUp/Pause

--- menuButton/giveUpButton.js
+++ menuButton/giveUpButton.js
@@ -3,10 +3,15 @@ function createGiveUpButton() {
     const giveUpButton   = document.createElement('input');
     giveUpButton.setAttribute('id','giveUpButton');
     giveUpButton.setAttribute('type','button');
-    giveUpButton.setAttribute('value','Give up ?');
-    giveUpButton.setAttribute('style','width:100%');
+    giveUpButton.setAttribute('value','Abandonner');
+    giveUpButton.setAttribute("style", "background-color:white");
+    giveUpButton.addEventListener('mouseover', getColorRed);
+    giveUpButton.addEventListener('mouseout', removeColor);
     giveUpButton.addEventListener('click', homePage);
     useDivForReset.appendChild(giveUpButton); 
 }
 
-createGiveUpButton();
+function removeGiveUpButton() {
+    const useDivForResetBis = document.querySelector('#div1');
+    useDivForResetBis.innerHTML = ('');
+}
--- menuButton/pauseButton.js
+++ menuButton/pauseButton.js
@@ -1,25 +1,26 @@
-const useDivForReset = document.querySelector('#div2');
 const pauseButton    = document.createElement('input');
 
 function createPauseButton() {
+    const useDiv2ForReset = document.querySelector('#div2');
     pauseButton.setAttribute('id','pauseButton');
     pauseButton.setAttribute('type','button');
-    pauseButton.setAttribute('value','Pause ?');
-    pauseButton.setAttribute('style','width:100%');
+    pauseButton.setAttribute('value','Pause');
+    pauseButton.setAttribute("style", "background-color:white");
+    pauseButton.addEventListener('mouseover', getColorYellow);
+    pauseButton.addEventListener('mouseout', removeColor);
     pauseButton.addEventListener('click', pauseButtonAction);
-    useDivForReset.appendChild(pauseButton); 
+    useDiv2ForReset.appendChild(pauseButton); 
 }
 
 function pauseInterval() {
     pause = true;
-    pauseButton.setAttribute('value','Continue ?');
+    pauseButton.setAttribute('value','Reprendre');
 }
 
 function resumeInterval() {
     pause = false;
-    pauseButton.setAttribute('value','Pause ?');
-    if     (gameMode == 1) challenge1Action();
-    else if(gameMode == 2) challenge2Action();
+    pauseButton.setAttribute('value','Pause');
+    challengeAction();
 }
 
 function pauseButtonAction() {
@@ -27,4 +28,7 @@ function pauseButtonAction() {
     else if (pause == true ) return resumeInterval();
 }
 
-createPauseButton();
+function removePauseButton() {
+    const useDiv2ForResetBis = document.querySelector('#div2');
+    useDiv2ForResetBis.innerHTML = ('');
+}
